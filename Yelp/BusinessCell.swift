d815eebf0d0399141421f9abe0d83d88133ca694
Arjit Jaiswal <arjitj2@gmail.com>
2016-02-14 11:39:06 -0600

    finished video 1

--- Yelp/BusinessCell.swift
+++ Yelp/BusinessCell.swift
@@ -10,9 +10,37 @@ import UIKit
 
 class BusinessCell: UITableViewCell {
 
+    @IBOutlet weak var thumbNailView: UIImageView!
+    
+    @IBOutlet weak var ratingImageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var distanceLabel: UILabel!
+    
+    @IBOutlet weak var reviewLabel: UILabel!
+        
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    @IBOutlet weak var cuisineLabel: UILabel!
+    
+    var business: Business! {
+        didSet {
+            nameLabel.text = business.name
+            thumbNailView.setImageWithURL(business.imageURL!)
+            cuisineLabel.text = business.categories
+            addressLabel.text = business.address
+            reviewLabel.text = "\(business.reviewCount!) Reviews"
+            ratingImageView.setImageWithURL(business.ratingImageURL!)
+            distanceLabel.text = business.distance
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        thumbNailView.layer.cornerRadius = 3
+        thumbNailView.clipsToBounds = true
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
